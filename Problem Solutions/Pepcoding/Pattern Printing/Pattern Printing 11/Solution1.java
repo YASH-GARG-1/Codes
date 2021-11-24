adba4315b94a6457bd71f42156976df1c9485254
YASH-GARG-1 <yashgarg5000@gmail.com>
2021-11-25 00:28:54 +0530

    Create Solution1.java

---
+++ Problem Solutions/Pepcoding/Pattern Printing/Pattern Printing 11/Solution1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution1{
+
+    public static void pattern11(int n)
+    {
+        int num = 1;
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1; j<=i;j++)
+            {
+                System.out.print(num+"\t");
+                num++;
+            }
+            System.out.print("\n");
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the value of N : ");
+        int n = scn.nextInt();
+        System.out.println("The pattern is : ");
+        pattern11(n);
+        scn.close();
+    }
+}   
