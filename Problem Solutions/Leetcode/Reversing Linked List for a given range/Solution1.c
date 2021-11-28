d9c5db12c7c2192e4e5db93ffa56229798e79a0c
YASH-GARG-1 <yashgarg5000@gmail.com>
2021-11-28 18:09:20 +0530

    Update Solution1.c

--- Problem Solutions/Leetcode/Reversing Linked List for a given range/Solution1.c
+++ Problem Solutions/Leetcode/Reversing Linked List for a given range/Solution1.c
@@ -92,6 +92,7 @@ struct ListNode* createList()
         {
             printf("Enter Data : ");
             scanf("%d",&newnode->val);
+            newnode->next = NULL;
             if(head == NULL)
             {
                 head = temp = newnode;
