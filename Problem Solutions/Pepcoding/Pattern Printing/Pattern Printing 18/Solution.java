e1a38d130a47a7af322698d3b1066d47d194901f
YASH-GARG-1 <yashgarg5000@gmail.com>
2021-12-01 00:18:32 +0530

    Update Solution.java

--- Problem Solutions/Pepcoding/Pattern Printing/Pattern Printing 18/Solution.java
+++ Problem Solutions/Pepcoding/Pattern Printing/Pattern Printing 18/Solution.java
@@ -13,26 +13,26 @@ public class Solution {
             System.out.print("*");
         } else {
             for (int i = 1; i <= n; i++) {
-                System.out.print("*	");
+                System.out.print("*\t");
             }
             System.out.print("\n");
             for (int i = 1; i < (n / 2); i++) {
                 for (int j = 1; j <= i; j++) {
-                    System.out.print("	");
+                    System.out.print("\t");
                 }
-                System.out.print("*	");
+                System.out.print("*\t");
                 for (int k = 1; k <= spc; k++) {
-                    System.out.print("	");
+                    System.out.print("\t");
                 }
                 spc -= 2;
                 System.out.println("*");
             }
             for (int i = (n / 2 + 1); i >= 1; i--) {
                 for (int j = 2; j <= i; j++) {
-                    System.out.print("	");
+                    System.out.print("\t");
                 }
                 for (int k = 1; k <= str; k++) {
-                    System.out.print("*	");
+                    System.out.print("*\t");
                 }
                 str += 2;
                 System.out.print("\n");
