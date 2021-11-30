1685b9f7503708235a7baefb202decc11757d528
YASH-GARG-1 <yashgarg5000@gmail.com>
2021-12-01 00:18:35 +0530

    Update Solution1.java

--- Problem Solutions/Pepcoding/Pattern Printing/Pattern Printing 17/Solution1.java
+++ Problem Solutions/Pepcoding/Pattern Printing/Pattern Printing 17/Solution1.java
@@ -12,23 +12,23 @@ public class Solution1 {
         } else {
             for (int i = 1; i <= (n / 2); i++) {
                 for (int j = 1; j <= (n / 2); j++) {
-                    System.out.print("	");
+                    System.out.print("\t");
                 }
                 for (int k = 1; k <= i; k++) {
-                    System.out.print("*	");
+                    System.out.print("*\t");
                 }
                 System.out.print("\n");
             }
             for (int i = 1; i <= n; i++) {
-                System.out.print("*	");
+                System.out.print("*\t");
             }
             System.out.print("\n");
             for (int i = n / 2; i >= 1; i--) {
                 for (int j = 1; j <= (n / 2); j++) {
-                    System.out.print("	");
+                    System.out.print("\t");
                 }
                 for (int k = 1; k <= i; k++) {
-                    System.out.print("*	");
+                    System.out.print("*\t");
                 }
                 System.out.print("\n");
             }
