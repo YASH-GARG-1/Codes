0c0b58d34e9697b8093721751e27cd989a54770c
YASH-GARG-1 <yashgarg5000@gmail.com>
2021-11-30 22:50:10 +0530

    Create Solution.java

---
+++ Problem Solutions/Pepcoding/Pattern Printing/Pattern Printing 18/Solution.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the Value of N : ");
+        int n = scn.nextInt();
+        int spc = n - 4;
+        int str = 1;
+        System.out.println("The Pattern is : \n");
+        if (n == 1) {
+            System.out.print("*");
+        } else {
+            for (int i = 1; i <= n; i++) {
+                System.out.print("*	");
+            }
+            System.out.print("\n");
+            for (int i = 1; i < (n / 2); i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("	");
+                }
+                System.out.print("*	");
+                for (int k = 1; k <= spc; k++) {
+                    System.out.print("	");
+                }
+                spc -= 2;
+                System.out.println("*");
+            }
+            for (int i = (n / 2 + 1); i >= 1; i--) {
+                for (int j = 2; j <= i; j++) {
+                    System.out.print("	");
+                }
+                for (int k = 1; k <= str; k++) {
+                    System.out.print("*	");
+                }
+                str += 2;
+                System.out.print("\n");
+            }
+        }
+        scn.close();
+    }
+}
